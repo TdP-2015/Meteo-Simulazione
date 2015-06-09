a42d5fb30faf7ae182a6b63103bc9f24a658e200
fcorno <fulvio.corno@polito.it>
2015-06-09 13:09:15 +0200

    Risoluzione Punto 1

--- src/it/polito/tdp/meteo/MeteoController.java
+++ src/it/polito/tdp/meteo/MeteoController.java
@@ -1,7 +1,15 @@
 package it.polito.tdp.meteo;
 
+import it.polito.tdp.meteo.bean.Model;
+
 import java.net.URL;
+import java.time.Month;
+import java.time.format.TextStyle;
+import java.util.Locale;
+import java.util.Map;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -9,6 +17,8 @@ import javafx.scene.control.TextArea;
 
 
 public class MeteoController {
+	
+	private Model model ;
 
     @FXML
     private ResourceBundle resources;
@@ -17,7 +27,7 @@ public class MeteoController {
     private URL location;
 
     @FXML
-    private ChoiceBox<?> boxMese;
+    private ChoiceBox<Month> boxMese;
 
     @FXML
     private Button btnCalcola;
@@ -27,6 +37,31 @@ public class MeteoController {
 
     @FXML
     private TextArea txtResult;
+    
+
+    @FXML
+    void doUmidita(ActionEvent event) {
+    	
+    	Month mese = boxMese.getValue() ;
+    	
+    	if(mese==null) {
+    		txtResult.appendText("Errore: specificare il mese\n"); 
+    		return ;
+    	}
+    	
+    	Map<String, Double> umidita = model.getUmiditaMedia(mese) ;
+    	
+    	txtResult.appendText(String.format("Mese: %s\n",mese.getDisplayName(TextStyle.FULL, Locale.ITALIAN))) ;
+    	for(String citta : umidita.keySet()) {
+    		txtResult.appendText(String.format("%s: %.2f\n", citta, umidita.get(citta))); 
+    	}
+
+    }
+
+    @FXML
+    void doCalcola(ActionEvent event) {
+
+    }
 
 
     @FXML
@@ -36,7 +71,13 @@ public class MeteoController {
         assert btnUmidita != null : "fx:id=\"btnUmidita\" was not injected: check your FXML file 'Meteo.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Meteo.fxml'.";
 
+        boxMese.getItems().clear() ;
+        boxMese.getItems().addAll(Month.values()) ;
 
+    }
+    
+    public void setModel(Model m) {
+    	this.model = m ;
     }
 
 }
